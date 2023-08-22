bd46795b5157d3af1b15cfe3252504e6e454f48c
Kasualix <670067575@qq.com>
2023-08-22 23:17:18 +0800

    1165

--- src/main/java/com/teampotato/bocchium/Bocchium.java
+++ src/main/java/com/teampotato/bocchium/Bocchium.java
@@ -7,17 +7,18 @@ import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.dimension.DimensionType;
-import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.network.NetworkConstants;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.lwjgl.glfw.GLFW;
 
 @Mod(Bocchium.MOD_ID)
@@ -44,7 +45,7 @@ public class Bocchium {
         if (!shouldCullBottomBedrock.get()) return false;
         ClientWorld clientWorld = minecraftClient.world;
         if (clientWorld == null) return false;
-        return facing == Direction.DOWN && height == clientWorld.getBottomY();
+        return facing == Direction.DOWN && height == 0;
     }
 
     public static boolean canCullTop(Direction facing, int height) {
@@ -58,7 +59,7 @@ public class Bocchium {
     public Bocchium() {
         ModLoadingContext ctx = ModLoadingContext.get();
         ctx.registerConfig(ModConfig.Type.CLIENT, config);
-        ctx.registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        ctx.registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         minecraftClient = MinecraftClient.getInstance();
         MinecraftForge.EVENT_BUS.register(KeyInput.class);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
