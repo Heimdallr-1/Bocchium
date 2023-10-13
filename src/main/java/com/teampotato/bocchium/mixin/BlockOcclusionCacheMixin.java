da32a6e74aac7f28af0a86fc414907a257b9baa1
Kasualix <670067575@qq.com>
2023-10-13 18:02:39 +0800

    1201

--- src/main/java/com/teampotato/bocchium/mixin/BlockOcclusionCacheMixin.java
+++ src/main/java/com/teampotato/bocchium/mixin/BlockOcclusionCacheMixin.java
@@ -1,11 +1,12 @@
 package com.teampotato.bocchium.mixin;
 
 import com.teampotato.bocchium.Bocchium;
-import me.jellysquid.mods.sodium.client.render.occlusion.BlockOcclusionCache;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockOcclusionCache;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(value = BlockOcclusionCache.class, remap = false)
 public abstract class BlockOcclusionCacheMixin {
     @Inject(method = "shouldDrawSide", at = @At("HEAD"), cancellable = true)
-    private void onShouldDrawSide(BlockState selfState, BlockView view, BlockPos pos, Direction facing, CallbackInfoReturnable<Boolean> cir) {
+    private void onShouldDrawSide(BlockState selfState, BlockView view, @NotNull BlockPos pos, Direction facing, CallbackInfoReturnable<Boolean> cir) {
         if (Bocchium.shouldCull(facing, pos.getY())) cir.setReturnValue(false);
     }
 }
