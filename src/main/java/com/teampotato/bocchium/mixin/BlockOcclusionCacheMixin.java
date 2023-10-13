5e4e6cd05dfe3ac649b90d4428fbdfd98f98a53e
Kasualix <670067575@qq.com>
2023-10-13 17:56:48 +0800

    0.0.2

--- src/main/java/com/teampotato/bocchium/mixin/BlockOcclusionCacheMixin.java
+++ src/main/java/com/teampotato/bocchium/mixin/BlockOcclusionCacheMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,10 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(value = BlockOcclusionCache.class, remap = false)
 public abstract class BlockOcclusionCacheMixin {
     @Inject(method = "shouldDrawSide", at = @At("HEAD"), cancellable = true)
-    private void onShouldDrawSide(BlockState selfState, BlockView view, BlockPos pos, Direction facing, CallbackInfoReturnable<Boolean> cir) {
-        if (Bocchium.shouldCull(facing, pos.getY())) {
-            cir.setReturnValue(false);
-            cir.cancel();
-        }
+    private void onShouldDrawSide(BlockState selfState, BlockView view, @NotNull BlockPos pos, Direction facing, CallbackInfoReturnable<Boolean> cir) {
+        if (Bocchium.shouldCull(facing, pos.getY())) cir.setReturnValue(false);
     }
 }
---
+++ src/main/java/com/teampotato/bocchium/mixin/SodiumGameOptionPagesMixin.java
@@ -0,0 +1,65 @@
+package com.teampotato.bocchium.mixin;
+
+import com.teampotato.bocchium.Bocchium;
+import me.jellysquid.mods.sodium.client.gui.SodiumGameOptionPages;
+import me.jellysquid.mods.sodium.client.gui.SodiumGameOptions;
+import me.jellysquid.mods.sodium.client.gui.options.*;
+import me.jellysquid.mods.sodium.client.gui.options.control.TickBoxControl;
+import me.jellysquid.mods.sodium.client.gui.options.storage.SodiumOptionsStorage;
+import net.minecraft.text.TranslatableText;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.List;
+
+@Mixin(value = SodiumGameOptionPages.class, remap = false)
+public abstract class SodiumGameOptionPagesMixin {
+    @Shadow @Final private static SodiumOptionsStorage sodiumOpts;
+
+    @Inject(method = "performance", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", remap = false, ordinal = 0, shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+    private static void insertSetting(CallbackInfoReturnable<OptionPage> cir, @NotNull List<OptionGroup> groups) {
+        OptionImpl<SodiumGameOptions, Boolean> enableBocchium = OptionImpl.createBuilder(Boolean.class, sodiumOpts)
+                .setName(new TranslatableText("bocchium.enable"))
+                .setTooltip(new TranslatableText("bocchium.desc"))
+                .setControl(TickBoxControl::new)
+                .setBinding(
+                        (option, value) -> Bocchium.enableBocchium.set(value),
+                        option -> Bocchium.enableBocchium.get()
+                )
+                .setImpact(OptionImpact.LOW)
+                .setFlags(new OptionFlag[]{OptionFlag.REQUIRES_RENDERER_RELOAD})
+                .build();
+
+        OptionImpl<SodiumGameOptions, Boolean> shouldCullTop = OptionImpl.createBuilder(Boolean.class, sodiumOpts)
+                .setName(new TranslatableText("bocchium.cull_top"))
+                .setTooltip(new TranslatableText("bocchium.cull_top.tooltip"))
+                .setControl(TickBoxControl::new)
+                .setBinding(
+                        (option, value) -> Bocchium.shouldCullTopBedrock.set(value),
+                        option -> Bocchium.shouldCullTopBedrock.get()
+                )
+                .setImpact(OptionImpact.LOW)
+                .setFlags(new OptionFlag[]{OptionFlag.REQUIRES_RENDERER_RELOAD})
+                .build();
+
+        OptionImpl<SodiumGameOptions, Boolean> shouldCullBottom = OptionImpl.createBuilder(Boolean.class, sodiumOpts)
+                .setName(new TranslatableText("bocchium.cull_bottom"))
+                .setTooltip(new TranslatableText("bocchium.cull_bottom.tooltip"))
+                .setControl(TickBoxControl::new)
+                .setBinding(
+                        (option, value) -> Bocchium.shouldCullBottomBedrock.set(value),
+                        option -> Bocchium.shouldCullBottomBedrock.get()
+                )
+                .setImpact(OptionImpact.LOW)
+                .setFlags(new OptionFlag[]{OptionFlag.REQUIRES_RENDERER_RELOAD})
+                .build();
+
+        groups.add(OptionGroup.createBuilder().add(enableBocchium).add(shouldCullTop).add(shouldCullBottom).build());
+    }
+}
